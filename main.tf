2991e1075647d7025f947628e282f16cba798f35
SNTESTBernie <beiboer@hotmail.com>
2024-12-27 17:12:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 resource "google_compute_network" "app" {
-  name                    = "var.network_name"
+  name                    = var.network_name
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "app" {
-  name          = "var.network_name"
-  ip_cidr_range = "var.network_ip_range"
-  region        = "var.var_region"
+  name          = var.network_name
+  ip_cidr_range = var.network_ip_range
+  region        = var.var_region
   network       = google_compute_network.app.id
 }
 
 data "google_compute_image" "ubuntu" {
   most_recent = true
-  project     = "var.image_project" 
-  family      = "var.image_family"
+  project     = var.image_project
+  family      = var.image_family
 }
 
 resource "google_compute_instance" "blog" {
-  name         = "var.app_name"
-  machine_type = "var.machine_type"
+  name         = var.app_name
+  machine_type = var.machine_type
 
    boot_disk {
     initialize_params {
@@ -26,7 +26,7 @@ resource "google_compute_instance" "blog" {
     }
   }
   network_interface {
-   subnetwork = "google_compute_subnetwork.app.name"
+   subnetwork = google_compute_subnetwork.app.name
    access_config {
       # Leave empty for dynamic public IP
     }
