178aa1ed3ee0dae732593e1ea7a577d97bb82fa2
SNTESTBernie <beiboer@hotmail.com>
2024-12-30 16:20:35 +0100

    website

--- main.tf
+++ main.tf
@@ -29,19 +29,6 @@ ingress_rules = [
   ]
 }
 
-
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = var.network_name
-  ip_cidr_range = var.network_ip_range
-  region        = var.region
-  network       = google_compute_network.app.id
-}
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = var.image_project
@@ -51,6 +38,8 @@ data "google_compute_image" "ubuntu" {
 resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.machine_type
+
+   tags = ["${var.network_name}-web"]
 
    boot_disk {
     initialize_params {
@@ -63,6 +52,6 @@ resource "google_compute_instance" "blog" {
       # Leave empty for dynamic public IP
     }
   }
-
+  metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
   allow_stopping_for_update = true
 }
